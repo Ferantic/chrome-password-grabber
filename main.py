f292adb8a4e4a722b793738143ac176ab90d9971
Ferantic <silvermo2010@gmail.com>
2025-06-15 14:28:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 import time
 
 # Your bot token and chat ID
-BOT_TOKEN = 'bot_token_here'
+BOT_TOKEN = '7909854177:AAEuFKww3A1W-ksvR4qBSgWIKtX09ibpGMI
 CHAT_ID = 'chat_id_here'
 
 def is_admin():
